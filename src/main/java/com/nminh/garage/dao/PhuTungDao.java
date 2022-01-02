1a86ef17869378717e7cfbedb9fc69fb9044ce3e
ha2320 <85424049+ha2320@users.noreply.github.com>
2022-01-02 15:43:48 +0700

    Sửa tên methods

--- src/main/java/com/nminh/garage/dao/PhuTungDao.java
+++ src/main/java/com/nminh/garage/dao/PhuTungDao.java
@@ -7,7 +7,8 @@ import java.util.List;
 public interface PhuTungDao {
     PhuTung themPhuTung(int nhaCungCapId,PhuTung pt);
     PhuTung suaPhuTung(int PhuTungId,PhuTung pt);
-    List<PhuTung> getListPhuTung();
+    List<PhuTung> getPhuTung(String tuKhoa);
+    List<PhuTung> getPhuTung();
     List<PhuTung> getListPhuTungTheoNhaCUngCap(String name);
     void xoaPhuTung(int phuTungId);
 }
